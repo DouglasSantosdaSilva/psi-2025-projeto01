2661c7dba0afadd554cd1e28acc37c1719aa284b
douglas <douglassantos99249@gmail.com>
2025-10-22 11:26:15 -0300

    atualizando o projeto

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Jogador
 
-# Register your models here.
+@admin.register(Jogador)
+class JogadorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'idade', 'posicao', 'nascimento')
+    search_fields = ('nome', 'posicao', 'nascimento')
